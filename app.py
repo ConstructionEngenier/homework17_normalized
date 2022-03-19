ca1bea70bbeb238e43d901daf2f15fde18b593f3
Artem <artem.mikhaylov5593@yandex.ru>
2022-03-19 21:31:18 +0300

    create schema_app.py fill app.py by creating routes for movies and movie by id

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 # app.py
 
 from flask import Flask, request
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema, fields
+from schema_app import MovieSchema, GenreSchema, DirectorSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+api = Api(app)
+
+movie_ns = api.namespace('movies')
 
 
 class Movie(db.Model):
@@ -24,6 +27,7 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +39,43 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('director_id')
+        parser.add_argument('genre_id')
+        params = parser.parse_args()
+
+        movies_schema = MovieSchema(many=True)
+        director_id = params['director_id']
+        genre_id = params['genre_id']
+        # director_id = request.args.get('director_id')
+        # genre_id = request.args.get('genre_id')
+        if director_id and genre_id:
+            movies = Movie.query.filter_by(director_id=director_id, genre_id=genre_id).all()
+        elif director_id:
+            movies = Movie.query.filter_by(director_id=director_id).all()
+        elif genre_id:
+            movies = Movie.query.filter_by(genre_id=genre_id).all()
+        else:
+            movies = Movie.query.all()
+        if movies:
+            return movies_schema.dump(movies), 200
+        else:
+            return "", 404
+
+
+@movie_ns.route('/<int:mid>')
+class MoviesView(Resource):
+    def get(self, mid: int):
+        try:
+            movie = Movie.query.get(mid)
+            movie_schema = MovieSchema()
+            return movie_schema.dump(movie), 200
+        except Exception as e:
+            return "", 404
 
 
 if __name__ == '__main__':
